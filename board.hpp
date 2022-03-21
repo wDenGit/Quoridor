5296e2c2aff22ddc9c22da5acd3a670e37fca58c
sazmar18 <Samir.azmar@ulb.be>
2022-03-21 19:00:50 +0100

    affichage et mouvement fonctionel

---
+++ board.hpp
@@ -0,0 +1,100 @@
+#ifndef BOARD_HPP
+#define BOARD_HPP
+
+#include <vector>
+#include <memory>
+#include <array>
+#include <iostream>     // DEBUG
+
+struct Point{
+    int x,y;
+};
+
+class Entity {
+    public:
+        Entity()=default;
+        virtual int getPlayer()=0;
+};
+class Wall: public Entity {
+    int player;
+    public:
+        Wall()=default;
+        int getPlayer(){return player;}
+};
+
+class Pawn: public Entity {
+    int player;
+    Point pos;
+    public:
+        Pawn(int player): player{player} {};
+        int getPlayer() {return player;}
+        void setPos(Point newPos) {this->pos = newPos;}
+        Point getPos() {return this->pos;};
+        
+};
+
+class Case {
+    std::shared_ptr<Entity> entity =nullptr;
+    Point pos;
+    std::vector<std::shared_ptr<Case>> pawnCaseNeighbor;
+    std::vector<std::shared_ptr<Case>> wallCaseNeighbor;
+    int caseType;
+    public:
+        enum caseT{wall, pawn};
+        Case(int type, int x, int y);
+
+        // Setter & Getter
+        void setPawnNeighbors(std::vector<std::shared_ptr<Case>> v) {pawnCaseNeighbor = v;}
+        void setWallNeighbors(std::vector<std::shared_ptr<Case>> v) {wallCaseNeighbor = v;}
+        std::vector<std::shared_ptr<Case>> getPawnCaseNeighbors() {return pawnCaseNeighbor;}
+        std::vector<std::shared_ptr<Case>> getWallCaseNeighbors() {return wallCaseNeighbor;}
+        
+        bool isEmpty() const {return entity == nullptr;}
+        bool isPawnCase() const {return caseType == pawn;}
+        void put(std::shared_ptr<Entity> newEntity);
+
+        int getPlayer_1(); // Recuperer le joueur
+
+        Point getPos() const {return pos;}
+
+        void setEmpty() {entity = nullptr;}
+};
+
+
+class Board {
+    std::vector<std::vector<std::shared_ptr<Case>>> board;
+    int size;
+    std::array<Point,4> pawns;
+
+    void initializeNeighbors();
+    public:
+        Board(int caseNumber=9);
+
+        const std::vector<std::vector<std::shared_ptr<Case>>> &getBoard() const {return board;}
+
+        size_t getSize() const {return board.size();}
+        
+        bool checkEmptyCase(Point pos) const;
+
+        bool checkCaseType(Point pos) const;
+
+        int checkWinner();
+
+        bool checkPath(std::vector<Point> point, Board board);
+
+        void putWall(std::shared_ptr<Wall> newWall, Point pos1, Point pos2, Point pos3);
+
+        void putPawn(std::shared_ptr<Pawn> newPawn, Point pos);
+
+        void removePawn(Point p_pawn);
+
+        void removeWall(Point pos1, Point pos2, Point pos3);
+        std::array<Point,4> getPawn() {return pawns;}         
+        
+        std::shared_ptr<Case> getCase(Point pos);
+
+        
+        
+}; 
+
+#endif
---
+++ move_management.hpp
@@ -0,0 +1,31 @@
+#ifndef MOVE_MANAGEMENT_HPP
+#define MOVE_MANAGEMENT_HPP
+
+#include "board.hpp"
+
+class ControlMove {
+    std::shared_ptr<Board> board;
+    public:
+        ControlMove(std::shared_ptr<Board> board) : board{board} {};
+
+        bool checkAlignment(std::shared_ptr<Case> case_main, std::shared_ptr<Case> neighborsPawnCase, std::shared_ptr<Case> neighborsWallCase);
+
+        bool isAligner(std::shared_ptr<Case> case1, std::shared_ptr<Case> case2, std::shared_ptr<Case> wall);
+
+        bool isWallBetween(std::shared_ptr<Case> pawnCase1, std::shared_ptr<Case> pawnCase2, std::shared_ptr<Case> wallCase);
+        
+        void printPos(std::vector<std::shared_ptr<Case>> vec);
+
+        std::vector<std::shared_ptr<Case>> checkMovement(std::shared_ptr<Case> case_main);
+                
+        void highlightPlayable(){}
+
+        
+        // void printNeighbors(Point pos);
+
+        std::vector<Point> sendMovePossible(Point p);
+        
+        // bool movePawn(Point pos_init, Point pos_dest);
+};
+
+#endif
